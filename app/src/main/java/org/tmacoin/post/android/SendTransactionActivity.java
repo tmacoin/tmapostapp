fede06d03b3bff5c8fda0a4845e5e14ff10dee91
Tma Coin <cointma@gmail.com>
2020-01-17 19:38:47 -0500

    send transaction screen
    Signed-off-by: Tma Coin <cointma@gmail.com>

--- app/src/main/java/org/tmacoin/post/android/SendTransactionActivity.java
+++ app/src/main/java/org/tmacoin/post/android/SendTransactionActivity.java
@@ -3,6 +3,9 @@ package org.tmacoin.post.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SendTransactionActivity extends BaseActivity {
 
@@ -10,5 +13,21 @@ public class SendTransactionActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_transaction);
+        Button sendTransactionButton = findViewById(R.id.sendTransactionButton);
+        sendTransactionButton.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+        sendTransactionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard(v);
+            }
+        });
+
     }
 }
