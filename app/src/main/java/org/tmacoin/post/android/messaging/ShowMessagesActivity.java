a7107a69c1b654767cb3067b8cfa68a8fe641c46
Tma Coin <cointma@gmail.com>
2020-01-19 21:35:48 -0500

    updated
    Signed-off-by: Tma Coin <cointma@gmail.com>

--- app/src/main/java/org/tmacoin/post/android/messaging/ShowMessagesActivity.java
+++ app/src/main/java/org/tmacoin/post/android/messaging/ShowMessagesActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -15,16 +16,25 @@ import org.tma.peer.Network;
 import org.tma.peer.thin.GetMessagesRequest;
 import org.tma.peer.thin.ResponseHolder;
 import org.tma.peer.thin.SecureMessage;
+import org.tma.util.Base58;
+import org.tma.util.Encryptor;
+import org.tma.util.StringUtil;
+import org.tma.util.TmaLogger;
 import org.tmacoin.post.android.AndroidExecutor;
 import org.tmacoin.post.android.BaseActivity;
 import org.tmacoin.post.android.R;
 import org.tmacoin.post.android.Wallets;
 
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 import java.security.PublicKey;
 import java.util.Iterator;
 import java.util.List;
 
 public class ShowMessagesActivity extends BaseActivity {
+
+    private static final TmaLogger logger = TmaLogger.getLogger();
+    private static final Encryptor encryptor = new Encryptor();
 
     private List<SecureMessage> list = null;
 
@@ -90,8 +100,34 @@ public class ShowMessagesActivity extends BaseActivity {
         }
         final ProgressBar pgsBar = findViewById(R.id.progressBar);
         pgsBar.setVisibility(View.INVISIBLE);
-        ListView simpleList = (ListView)findViewById(R.id.simpleListView);
+        ListView listView = (ListView)findViewById(R.id.simpleListView);
         MessageAdapter arrayAdapter = new MessageAdapter(this, list);
-        simpleList.setAdapter(arrayAdapter);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SecureMessage secureMessage = (SecureMessage) parent.getItemAtPosition(position);
+                Toast.makeText(getBaseContext(), getBody(secureMessage), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private String getBody(SecureMessage secureMessage) {
+        Wallet wallet = Wallets.getInstance().getWallet(Wallets.TMA, Wallets.WALLET_NAME);
+        if(!secureMessage.getRecipient().equals(wallet.getTmaAddress())) {
+            return "";
+        }
+        try {
+            String str = StringUtil.trimToNull(secureMessage.getText());
+            if(str != null) {
+                str = new String(encryptor.decryptAsymm(Base58.decode(str), wallet.getPrivateKey()), StandardCharsets.UTF_8);
+                int index = str.indexOf("\n");
+                index = index == -1? str.length(): index;
+                return  str.substring(index + 1);
+            }
+        } catch (GeneralSecurityException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return "";
     }
 }
