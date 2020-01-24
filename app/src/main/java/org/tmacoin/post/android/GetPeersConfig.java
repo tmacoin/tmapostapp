47b7b7484c696f432f5071b573f83290303722f6
javatodesign <javatodesign@gmail.com>
2020-01-24 08:36:04 -0500

    Screen and class for get peers.config file

---
+++ app/src/main/java/org/tmacoin/post/android/GetPeersConfig.java
@@ -0,0 +1,12 @@
+package org.tmacoin.post.android;
+
+import android.os.Bundle;
+
+public class GetPeersConfig extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_peers_config);
+    }
+}
