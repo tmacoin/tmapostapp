4a7869f65c6df77d919d022b958fbb0595bd474c
Tma Coin <cointma@gmail.com>
2020-02-04 23:58:23 -0500

    improved performance of log viewer
    Signed-off-by: Tma Coin <cointma@gmail.com>

--- app/src/main/java/org/tmacoin/post/android/LogViewerActivity.java
+++ app/src/main/java/org/tmacoin/post/android/LogViewerActivity.java
@@ -1,10 +1,13 @@
 package org.tmacoin.post.android;
 
 import android.os.Bundle;
+import android.os.FileObserver;
 import android.os.Handler;
 import android.os.Looper;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 import org.tma.util.Constants;
 import org.tma.util.StringUtil;
@@ -12,14 +15,20 @@ import org.tma.util.ThreadExecutor;
 import org.tma.util.TmaLogger;
 import org.tma.util.TmaRunnable;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 public class LogViewerActivity extends BaseActivity {
 
     private static final TmaLogger logger = TmaLogger.getLogger();
 
     private long pointer;
+    private FileObserver observer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,39 +42,41 @@ public class LogViewerActivity extends BaseActivity {
             return;
         }
 
+        readLog(log);
 
+        observer = new FileObserver(log.getAbsolutePath(), FileObserver.MODIFY) {
 
-        ThreadExecutor.getInstance().execute(new TmaRunnable("LogViewerActivity") {
-            public void doRun() {
-                while(true) {
-                    ThreadExecutor.sleep(Constants.ONE_SECOND);
-                    readLog(log);
-                    if(!logContent.isShown()) {
-                        logger.debug("Exiting LogViewerActivity Loop");
-                        break;
-                    }
-                }
+            @Override
+            public void onEvent(int event, @Nullable String path) {
+                readLog(log);
             }
-        });
+        };
+
+        observer.startWatching();
 
     }
 
     private void readLog(File file) {
         final StringBuilder builder = new StringBuilder();
         RandomAccessFile randomAccessFile = null;
+        FileInputStream fis = null;
+        ByteArrayOutputStream result = null;
         try {
             randomAccessFile = new RandomAccessFile(file, "r");
             long fileLength = file.length() - 1;
             if(pointer == 0) {
                 pointer = Math.max(0, fileLength - 10000);
             }
-            while(pointer < fileLength) {
-                pointer++;
-                randomAccessFile.seek(pointer);
-                char c = (char) randomAccessFile.read();
-                builder.append(c);
-            }
+            randomAccessFile.seek(pointer);
 
+            fis = new FileInputStream(randomAccessFile.getFD());
+
+            result = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
 
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
@@ -77,12 +88,26 @@ public class LogViewerActivity extends BaseActivity {
                     logger.error(e.getMessage(), e);
                 }
             }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (Exception e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
         }
 
-        final String text = builder.toString();
-        if(StringUtil.isEmpty(text)) {
+        final String text;
+        try {
+            text = result.toString(StandardCharsets.UTF_8.name());
+            if(StringUtil.isEmpty(text)) {
+                return;
+            }
+        } catch (UnsupportedEncodingException e) {
+            logger.error(e.getMessage(), e);
             return;
         }
+
 
         new Handler(Looper.getMainLooper()).post(new Runnable(){
             @Override
