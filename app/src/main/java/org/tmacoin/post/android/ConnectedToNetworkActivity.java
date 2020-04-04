29698170f0db034c00db95fbe2065e56ec7e5b95
Tma Coin <cointma@gmail.com>
2020-04-04 12:26:07 -0400

    updated
    Signed-off-by: Tma Coin <cointma@gmail.com>

--- app/src/main/java/org/tmacoin/post/android/ConnectedToNetworkActivity.java
+++ app/src/main/java/org/tmacoin/post/android/ConnectedToNetworkActivity.java
@@ -6,12 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.tma.blockchain.Transaction;
+import org.tma.blockchain.TransactionData;
+import org.tma.blockchain.TransactionOutput;
 import org.tma.blockchain.Wallet;
 import org.tma.peer.Network;
+import org.tma.peer.SendTransactionRequest;
 import org.tma.peer.thin.GetBalanceRequest;
 import org.tma.peer.thin.GetFaucetRequest;
+import org.tma.peer.thin.GetInputsRequest;
 import org.tma.peer.thin.NewMessageEvent;
 import org.tma.peer.thin.ResponseHolder;
+import org.tma.util.Coin;
+import org.tma.util.Constants;
 import org.tma.util.Listeners;
 import org.tma.util.ThreadExecutor;
 import org.tma.util.TmaLogger;
@@ -20,11 +27,15 @@ import org.tmacoin.post.android.messaging.NewMessageEventListener;
 import org.tmacoin.post.android.messaging.NewMessageNotifier;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class ConnectedToNetworkActivity extends BaseActivity {
 
     private static final TmaLogger logger = TmaLogger.getLogger();
     private static final Listeners listeners = Listeners.getInstance();
+    private static final String faucetAddress = "5KCJSsDpXkLiKv5juGoHuoaDbnvr2FASerm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,10 +63,35 @@ public class ConnectedToNetworkActivity extends BaseActivity {
                 if(!BigDecimal.ZERO.equals(amount)) {
                     return;
                 }
-                GetFaucetRequest getFaucetRequest = new GetFaucetRequest(network, network.getTmaAddress(), "5KCJSsDpXkLiKv5juGoHuoaDbnvr2FASerm");
+                GetFaucetRequest getFaucetRequest = new GetFaucetRequest(network, network.getTmaAddress(), faucetAddress);
                 getFaucetRequest.start();
+                ThreadExecutor.sleep(Constants.TIMEOUT);
+                sendTransaction();
             }
         });
+    }
+
+    private void sendTransaction() {
+        Network network = Network.getInstance();
+        final String tmaAddress = network.getTmaAddress();
+        final Coin total = Coin.SATOSHI.multiply(2);
+        final Wallet wallet = Wallets.getInstance().getWallet(Wallets.TMA, Wallets.WALLET_NAME);
+        TmaAndroidUtil.checkNetwork();
+        List<Coin> totals = new ArrayList<>();
+        totals.add(total);
+        List<Set<TransactionOutput>> inputList = new GetInputsRequest(network, tmaAddress, totals).getInputlist();
+        int i = 0;
+
+        if(inputList == null || inputList.size() != totals.size()) {
+            return;
+        }
+
+        Set<TransactionOutput> inputs = inputList.get(i++);
+        logger.debug("number of inputs: {} for {}", inputs.size(), tmaAddress);
+        Transaction transaction = new Transaction(wallet.getPublicKey(), faucetAddress, Coin.SATOSHI,
+                Coin.SATOSHI, inputs, wallet.getPrivateKey(), null, null, null);
+        logger.debug("sent {}", transaction);
+        new SendTransactionRequest(Network.getInstance(), transaction).start();
     }
 
     private void startNotifier() {
